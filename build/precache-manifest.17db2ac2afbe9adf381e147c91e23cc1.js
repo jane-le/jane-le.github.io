73d3c3ff41718e7f6f9e9a30944ace2d1eedddd3
jane-le <janele913@gmail.com>
2020-01-04 21:58:29 -0500

    production buiild

---
+++ build/precache-manifest.17db2ac2afbe9adf381e147c91e23cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "017dbe4bb6fc2087f6acb3346a7bba84",
+    "url": "/jane-le.github.io/static/media/portrait.017dbe4b.jpg"
+  },
+  {
+    "revision": "982e91fd81fbc671622b",
+    "url": "/jane-le.github.io/static/js/runtime~main.189b5152.js"
+  },
+  {
+    "revision": "eb9f197016a24d2586dd",
+    "url": "/jane-le.github.io/static/js/main.233e6f8f.chunk.js"
+  },
+  {
+    "revision": "7b15ec88502f58d31943",
+    "url": "/jane-le.github.io/static/js/2.3f89ad03.chunk.js"
+  },
+  {
+    "revision": "eb9f197016a24d2586dd",
+    "url": "/jane-le.github.io/static/css/main.7992a458.chunk.css"
+  },
+  {
+    "revision": "7b15ec88502f58d31943",
+    "url": "/jane-le.github.io/static/css/2.f36b7e2f.chunk.css"
+  },
+  {
+    "revision": "a7c89460aa194c2e1d7d4b8db82df40f",
+    "url": "/jane-le.github.io/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.fc58f42608fdc0072a53af3d1d960345.js"
+  "/jane-le.github.io/precache-manifest.17db2ac2afbe9adf381e147c91e23cc1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/jane-le.github.io/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
