4b40194b1fd2ed4afd6521e9a2fd58a1a139cab6
jane-le <janele913@gmail.com>
2020-01-15 19:07:52 -0500

    change profile pic

---
+++ src/components/Cloud.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Cloud extends Component {
+    render() {
+        const {
+            fill,
+        } = this.props;
+        return(
+        );
+    }
+}
+
+export default Cloud
