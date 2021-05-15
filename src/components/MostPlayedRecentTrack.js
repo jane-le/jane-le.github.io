59e7ee461de244a4e23e23e8cc5a06cf1974d568
jane-le <janele913@gmail.com>
2021-05-15 05:03:12 -0400

    use last.fm instead

--- src/components/MostPlayedRecentTrack.js
+++ src/components/MostPlayedRecentTrack.js
@@ -3,9 +3,8 @@ import React,  { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, Row, Col, Image} from 'react-bootstrap';
 
-const spotifyApiLink = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=1&offset=0'
-const oAuthToken = 'BQDCp2kAbRDOcUCs9PZygaEhkrhs3BnfD2bjbDKrNbz715FKrqQQjIF_nQlhCKALcEuWIozLmvEAocvjgBS2YZmIOSVqvaaf9zGlp_Y4LIgQTJ11p27HP28MsussVd7Bb28_IB7a3abS1_zypWxtHmcVD7OBhL7oGFW74Xez7HacLDSOAng';
-
+const apiLink = 'https://ws.audioscrobbler.com/2.0';
+const apiKey = '800de91cb1adb48853ce5fcd629b7b4a';
 
 function MostPlayedRecentTrack(props) {
     const styles = {
@@ -38,27 +37,29 @@ function MostPlayedRecentTrack(props) {
     useEffect(() => {
       axios({
         method: 'GET',
-        url: spotifyApiLink,
-        headers: {
-          'Authorization': `Bearer ${oAuthToken}`,
-          "Accept": "application/json",
-          "Content-Type": "application/json",
+        url: apiLink,
+        params: {
+          method: 'user.gettoptracks',
+          user: 'Janelamle',
+          limit: '1',
+          api_key: apiKey,
+          period: '7day',
+          format: 'json'
         },
       })
         .then((res) => {
-          console.log(res)
-          const trackInfo = res.data.items[0];
+          const trackInfo = res.data.toptracks.track[0];
+          console.log(trackInfo)
           const newTrack = {
             name: trackInfo.name,
-            artwork: trackInfo.album.images[0].url,
-            artist: trackInfo.artists[0].name,
-            spotifyLink: trackInfo.external_urls.spotify,
+            artwork: trackInfo.image[0]['#text'],
+            artist: trackInfo.artist.name,
           }
-          setTrack(newTrack);
+          setTrack(newTrack); 
         })
         .catch(err => {
           console.log(err);
-        })
+        }) 
     }, []);
   
     if (!track) {
@@ -66,10 +67,10 @@ function MostPlayedRecentTrack(props) {
     }
     return (
       <Container style={styles.main}>
-        <h2 style={styles.title}>Fun fact! My most played track on Spotify this past month was:</h2>
+        <h2 style={styles.title}>Fun fact! My most played track on Spotify this past week was:</h2>
         <Row >
           <Col style={{padding: '2rem 2rem 0 2rem'}} md={3}><Image src={track.artwork} fluid/></Col>
-          <Col style={styles.colCenter} md={9}><a href={track.spotifyLink}><h2 style={styles.name}>{track.name}</h2></a><h3 style={styles.title}>{track.artist}</h3></Col>
+          <Col style={styles.colCenter} md={9}><h2 style={styles.name}>{track.name}</h2><h3 style={styles.title}>{track.artist}</h3></Col>
         </Row>
       </Container>
     );
